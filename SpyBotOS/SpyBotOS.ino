975415a8bb5f4a4c42ea839a4c5536a9297be28e
Erik Schluntz <eschluntz@college.harvard.edu>
2012-04-27 17:01:12 -0400

    new version

--- SpyBotOS/SpyBotOS.ino
+++ SpyBotOS/SpyBotOS.ino
@@ -16,6 +16,15 @@ const int led1Pin = 5;
 const int led2Pin = 6;
 const int servoPin = 9;
 
+// motors
+
+const int rightMPin = 3;
+const int leftMPin = 11;
+
+const int rightDPin = 12; 
+const int leftDPin = 13;
+
+
 // array of data controlled by the anrdoid
 //--------------------------------------------------------------
 long time;
@@ -30,8 +39,11 @@ const int led2 = 11;
 const int led3 = 12;
 
 const int servo = 20;
+
+// motor inputs from android
 const int rightM = 21;
 const int leftM = 22;
+
 
 
 //--------------------------------------------------------------
@@ -50,6 +62,11 @@ void setup()
   // initialize the ledPin as an output:
   pinMode(led1Pin, OUTPUT);
   pinMode(led2Pin, OUTPUT);
+  pinMode(rightMPin, OUTPUT);
+  pinMode(leftMPin, OUTPUT);
+  pinMode(rightDPin, OUTPUT);
+  pinMode(leftDPin, OUTPUT);
+  
   servo1.attach(servoPin);
 }
 
@@ -61,11 +78,46 @@ void loop() {
   checkConnection();
   
   // normal loop
+  drive();
   analogWrite(led1Pin, data[led1]);
   analogWrite(led2Pin, data[led2]);
   servo1.write(data[servo]);
 
     
+}
+
+// drives the motors
+//--------------------------------------------------------------
+void drive() {
+  int rightm = data[rightM];
+  int leftm = data[leftM];
+  
+  // shifting to -128, 128
+  rightm = rightm - 128;
+  leftm = leftm - 128;
+  
+  // scaling to -256 to 256
+  rightm = 2*rightm;
+  leftm = 2*leftm;
+  
+  // setting booleans
+  boolean rightd = false;
+  boolean leftd = false;
+  if (rightm > 0) rightd = true;
+  if (leftm > 0) leftd = true;
+  
+  rightm = abs(rightm);
+  leftm = abs(leftm);
+  
+  // actually driving motors 
+  if (rightd) digitalWrite(rightDPin, HIGH);
+  else digitalWrite(rightDPin, LOW);
+  
+  if (leftd) digitalWrite(leftDPin, HIGH);
+  else digitalWrite(leftDPin, LOW);
+  
+  analogWrite(rightMPin, rightm);
+  analogWrite(leftMPin, leftm);
 }
 
 // inputs data from the android
@@ -93,6 +145,8 @@ void checkConnection() {
   if (millis() - time > 1000) {
     data[led1] = 0;
     data[led2] = 0;
+    data[rightM] = 128;
+    data[leftM] = 128;
     
     time = millis();
     waiting = true;
